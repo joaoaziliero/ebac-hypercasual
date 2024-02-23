1a19e6c9f0280ec697fe54b628c14d98b49285f7
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-02-23 17:17:05 -0300

    add management for obstacle interactions

---
+++ Assets/Scripts/Enemies/EnemyCollisions.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using R3;
+using R3.Triggers;
+
+public class EnemyCollisions : MonoBehaviour
+{
+    [SerializeField] private string _tagForEnemies;
+
+    private void Start()
+    {
+        ManageEnemyInteractions(
+            collider: GetComponent<Collider>(),
+            player: GetComponent<PlayerMotionControl>(),
+            tag: _tagForEnemies
+            ).AddTo(this);
+    }
+
+    private IDisposable ManageEnemyInteractions(Collider collider, string tag, PlayerMotionControl player)
+    {
+        return collider
+            .OnCollisionEnterAsObservable()
+            .Where(collision => collision.gameObject.CompareTag(tag))
+            .Select<Collision, Action>(collision => () => { player.enabled = false; })
+            .Subscribe(action => action());
+    }
+}
