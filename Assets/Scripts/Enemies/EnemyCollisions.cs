a955ead91980b3837048a58489db86c322f2f946
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-02-23 20:33:09 -0300

    Update EnemyCollisions.cs

--- Assets/Scripts/Enemies/EnemyCollisions.cs
+++ Assets/Scripts/Enemies/EnemyCollisions.cs
@@ -8,22 +8,25 @@ using R3.Triggers;
 public class EnemyCollisions : MonoBehaviour
 {
     [SerializeField] private string _tagForEnemies;
+    public GameObject SCREEN_Restart;
 
     private void Start()
     {
         ManageEnemyInteractions(
             collider: GetComponent<Collider>(),
             player: GetComponent<PlayerMotionControl>(),
+            auto: GetComponent<AutoPlayerMotion>(),
             tag: _tagForEnemies
             ).AddTo(this);
     }
 
-    private IDisposable ManageEnemyInteractions(Collider collider, string tag, PlayerMotionControl player)
+    private IDisposable ManageEnemyInteractions(Collider collider, string tag, PlayerMotionControl player, AutoPlayerMotion auto)
     {
         return collider
             .OnCollisionEnterAsObservable()
             .Where(collision => collision.gameObject.CompareTag(tag))
-            .Select<Collision, Action>(collision => () => { player.enabled = false; })
+            .Select<Collision, Action>(collision => () => { player.enabled = false; auto.enabled = false;
+                SCREEN_Restart.SetActive(true); })
             .Subscribe(action => action());
     }
 }
