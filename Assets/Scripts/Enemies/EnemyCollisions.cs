a0841124a6f41d41f0ab0b53c2791efdf840ff5e
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-03-08 19:19:51 -0300

    Update EnemyCollisions.cs

--- Assets/Scripts/Enemies/EnemyCollisions.cs
+++ Assets/Scripts/Enemies/EnemyCollisions.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 using System;
 using R3;
 using R3.Triggers;
+using DG.Tweening;
 
 public class EnemyCollisions : MonoBehaviour
 {
     [SerializeField] private string _tagForEnemies;
-    public GameObject SCREEN_Restart;
+    [SerializeField] private string _deathAnimationTrigger;
 
     private void Start()
     {
         ManageEnemyInteractions(
             collider: GetComponent<Collider>(),
-            player: GetComponent<PlayerMotionControl>(),
-            auto: GetComponent<AutoPlayerMotion>(),
-            tag: _tagForEnemies
-            ).AddTo(this);
+            tagForEnemies: _tagForEnemies,
+            transform: transform,
+            animator: GetComponentInChildren<Animator>(),
+            name: _deathAnimationTrigger)
+            .AddTo(this);
     }
 
-    private IDisposable ManageEnemyInteractions(Collider collider, string tag, PlayerMotionControl player, AutoPlayerMotion auto)
+    private IDisposable ManageEnemyInteractions(Collider collider, string tagForEnemies, Transform transform, Animator animator, string name)
     {
         return collider
             .OnCollisionEnterAsObservable()
-            .Where(collision => collision.gameObject.CompareTag(tag))
-            .Select<Collision, Action>(collision => () => { player.enabled = false; auto.enabled = false;
-                SCREEN_Restart.SetActive(true); })
-            .Subscribe(action => action());
+            .Where(collision => collision.gameObject.CompareTag(tagForEnemies))
+            .Subscribe(_ => { NudgeBackwards(transform); PlayAnimation(animator, name); });
     }
+
+    private readonly Action<Transform> NudgeBackwards = (transform) => { transform.DOMoveZ(-3, 1).SetRelative(true); };
+    private readonly Action<Animator, string> PlayAnimation = (animator, name) => { animator.SetTrigger(name); };
 }
