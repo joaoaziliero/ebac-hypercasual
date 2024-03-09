a4f45e1de39ba50391d72f17ca3d42499a45721c
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-03-09 14:36:49 -0300

    Update ApplyAnimatorSequence.cs

--- Assets/Scripts/Animator/ApplyAnimatorSequence.cs
+++ Assets/Scripts/Animator/ApplyAnimatorSequence.cs
@@ -3,22 +3,29 @@ using R3.Triggers;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ApplyAnimatorSequence : MonoBehaviour
 {
-    [SerializeField] private string _firstAnimationTrigger;
-    [SerializeField] private string _lastAnimationTrigger;
+    [SerializeField] private List<string> AnimationTriggerNames;
+    [SerializeField] private List<float> AnimationTriggerIntervals;
 
     private void OnEnable()
     {
-        PlayAnimation(transform.parent.GetComponentInChildren<Animator>(), _firstAnimationTrigger);
+        CreateAnimatorSequence(
+            names: AnimationTriggerNames,
+            intervals: AnimationTriggerIntervals,
+            animator: transform.parent.GetComponentInChildren<Animator>())
+            .ForEach(observable => observable.Subscribe());
+
+        Destroy(this.gameObject);
     }
 
-    private void OnDestroy()
+    private List<Observable<Unit>> CreateAnimatorSequence(List<string> names, List<float> intervals, Animator animator)
     {
-        PlayAnimation(transform.parent.GetComponentInChildren<Animator>(), _lastAnimationTrigger);
+        return names
+            .Select((name, i) => Observable.Timer(TimeSpan.FromSeconds(intervals[i])).Do(onCompleted: _ => animator.SetTrigger(name)))
+            .ToList();
     }
-
-    private readonly Action<Animator, string> PlayAnimation = (animator, name) => { animator.SetTrigger(name); };
 }
