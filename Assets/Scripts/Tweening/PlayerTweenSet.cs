fab76bb1b069b6e8a47707a72ce52aff05cf9c00
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-03-22 11:56:56 -0300

    add player tweening management

---
+++ Assets/Scripts/Tweening/PlayerTweenSet.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using R3;
+using R3.Triggers;
+using DG.Tweening;
+using System;
+
+public class PlayerTweenSet : MonoBehaviour
+{
+    [SerializeField] private string _tagForPowerUps;
+    [SerializeField] private float _tweenDuration;
+    [SerializeField] private Ease _ease;
+
+    private void Awake()
+    {
+        transform.DOScale(Vector3.zero, 1).From().SetDelay(1);
+    }
+
+    private void Start()
+    {
+        ManagePowerUpContact
+            (
+            playerCollider: GetComponentInParent<Collider>(),
+            tagForPowerUps: _tagForPowerUps,
+            transform: GetComponent<Transform>(),
+            tweenDuration: _tweenDuration,
+            ease: _ease
+            )
+            .AddTo(this);
+    }
+
+    private IDisposable ManagePowerUpContact(Collider playerCollider, string tagForPowerUps, Transform transform, float tweenDuration, Ease ease)
+    {
+        return playerCollider
+            .OnTriggerEnterAsObservable()
+            .Where(collider => collider.gameObject.CompareTag(tagForPowerUps))
+            .Subscribe(_ => 
+            {
+                transform.DOComplete();
+                transform.DOScale(1.2f, tweenDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+            });
+    }
+}
